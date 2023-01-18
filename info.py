7720e82d420f9b00eb72c129a80fd56b1f4cc851
Parthanp244 <122601245+Parthanp244@users.noreply.github.com>
2023-01-18 12:00:19 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 import os
 from os import environ
 
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_KEY = environ.get("API_KEY", "")
+API_ID = "5948230"
+API_HASH = "dd19a00b085a219421a3717d0ae9c0d0"
+BOT_TOKEN = "5231238723:AAFppyPBD3VKzj_-kHMXquwEVv1fAcOJCl4"
+API_KEY =  "e4804daf"
