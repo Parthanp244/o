ca8144988c7405e98fd84af97d6b22a500c24b49
Parthanp244 <122601245+Parthanp244@users.noreply.github.com>
2023-01-18 18:17:37 +0600

    Update OMDB.py

--- OMDB.py
+++ OMDB.py
@@ -7,7 +7,7 @@ user = {"User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) App
 
 def get_movie_info(query):    
     try:
-       url = f'https://varsity22.aparsclassroom.com/api/live/today?ajk=1/{query}/2023'
+       url = f'https://varsity22.aparsclassroom.com/api/live/today?ajk=1/{query}/2023'['Class']
        resp = requests.get(url).json()
        poster=resp['thumbnail_path']
        id=resp['Paper']
